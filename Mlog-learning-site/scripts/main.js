cc2906de5bf28c4aa6871a2059f95c75e2935808
TyT-xexebe <123269617+TyT-xexebe@users.noreply.github.com>
2023-04-25 16:13:40 +0300

    last

--- Mlog-learning-site/scripts/main.js
+++ Mlog-learning-site/scripts/main.js
@@ -1,24 +1,36 @@
-// JavaScript function to copy text to clipboard
+
 function copyToClipboard1(text1) {
-    // Get the text to copy
     var text = document.getElementById(text1).innerHTML;
   
-    // Create a new textarea element to hold the text
     var textarea = document.createElement("textarea");
     textarea.value = text;
     textarea.setAttribute("readonly", "");
     textarea.style.position = "absolute";
     textarea.style.left = "-9999px";
   
-    // Add the textarea to the document
     document.body.appendChild(textarea);
   
-    // Select the text in the textarea
     textarea.select();
   
-    // Copy the selected text to the clipboard
     document.execCommand("copy");
   
-    // Remove the textarea from the document
+    document.body.removeChild(textarea);
+  }
+
+  function copyToClipboard2(text2) {
+    var text = document.getElementById(text2).innerHTML;
+  
+    var textarea = document.createElement("textarea");
+    textarea.value = text;
+    textarea.setAttribute("readonly", "");
+    textarea.style.position = "absolute";
+    textarea.style.left = "-9999px";
+  
+    document.body.appendChild(textarea);
+  
+    textarea.select();
+  
+    document.execCommand("copy");
+  
     document.body.removeChild(textarea);
   }
